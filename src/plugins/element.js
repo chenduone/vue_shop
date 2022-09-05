33d60c15f688220f97a2e7a37d170047e0968ba8
lili <1124955977@qq.com>
2022-09-05 21:36:55 +0800

    完成订单功能开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -37,6 +37,8 @@ import {
     Checkbox,
     CheckboxGroup,
     Upload,
+    Timeline,
+    TimelineItem,
 } from 'element-ui'
 import { options } from 'less'
 
@@ -75,5 +77,7 @@ Vue.use(Steps)
 Vue.use(Checkbox)
 Vue.use(CheckboxGroup)
 Vue.use(Upload)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
